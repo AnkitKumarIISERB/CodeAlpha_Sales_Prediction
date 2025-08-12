008a8595cd86fdc9bb4903a900d39f5555a93ed9
AnkitKumarIISERB <ankittydvv123@gmail.com>
2025-08-12 12:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Load your trained model
+
 model = pickle.load(open('best_sales_model.pkl', 'rb'))
 
 @app.route('/')
@@ -13,18 +13,19 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get input values
+    
     tv = float(request.form['TV'])
     radio = float(request.form['Radio'])
     newspaper = float(request.form['Newspaper'])
 
-    # Make prediction
+    
     prediction = model.predict(np.array([[tv, radio, newspaper]]))[0]
     output = round(prediction, 2)
 
-    # Pass the prediction with a fade-in class
+   
     return render_template('index.html',
                            prediction_text=f"💡 Predicted Sales: {output} units")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
